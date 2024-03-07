deef49c8fcb45936f58ab89812178508b88a48d3
fabiconcept <favourajokubi@gmail.com>
2024-03-07 15:14:16 +0100

    Implement nav link click to close mobile nav

--- app/sections/NavSection.tsx
+++ app/sections/NavSection.tsx
@@ -54,11 +54,11 @@ export default function NavSection() {
                 ${openNav ? "left-0" : "left-[100%]"}
             `}>
                     <div className="w-full h-full text-xl text-center p-[5rem] grid gap-8 place-content-center">
-                        <Link href={"#service"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Services</Link>
-                        <Link href={"#whyus"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Why us</Link>
-                        <Link href={"#easysteps"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Easy steps</Link>
-                        <Link href={"#clientsreviews"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Clients reviews</Link>
-                        <Link href={"https://goldwater.floify.com"} target="_blank" className="bg-orange-400 smooth text-white p-8 py-3 font-semibold cursor-pointer active:scale-90 hover:scale-105 select-none sm:hidden block">
+                        <Link onClick={()=>setOpenNav(false)} href={"#service"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Services</Link>
+                        <Link onClick={()=>setOpenNav(false)} href={"#whyus"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Why us</Link>
+                        <Link onClick={()=>setOpenNav(false)} href={"#easysteps"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Easy steps</Link>
+                        <Link onClick={()=>setOpenNav(false)} href={"#clientsreviews"} className="px-8 hover:font-semibold smooth whitespace-nowrap cursor-pointer active:scale-90 hover:text-orange-400">Clients reviews</Link>
+                        <Link onClick={()=>setOpenNav(false)} href={"https://goldwater.floify.com"} target="_blank" className="bg-orange-400 smooth text-white p-8 py-3 font-semibold cursor-pointer active:scale-90 hover:scale-105 select-none sm:hidden block">
                             Apply now
                         </Link>
                     </div>
